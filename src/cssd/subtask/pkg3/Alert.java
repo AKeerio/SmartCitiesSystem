09c3959069e4d0e186b1f0066df35d7965458e48
Alaskan Tigerfish <louisken@hotmail.co.uk>
2018-01-25 10:33:12 +0000

    Changed sensorId to string.

--- src/cssd/subtask/pkg3/Alert.java
+++ src/cssd/subtask/pkg3/Alert.java
@@ -1,25 +1,28 @@
 package cssd.subtask.pkg3;
 import java.sql.Timestamp;
 
+// Louis
+
 public class Alert {
     
-    private int sensorId;
+    private String sensorId;
     private Timestamp time;
     
     public void attachTimestamp(Timestamp time) {
         this.time = time;
     }
     
-    public void attachSensorId(int sensorId) {
+    public void attachSensorId(String sensorId) {
         this.sensorId = sensorId;
     }
     
-    public int getSensorId() {
+    public String getSensorId() {
         return this.sensorId;
     }
     
     public Timestamp getTimestamp() {
         return this.time;
     }
-    
+
 }
+
