8e0a70b5d50d7d1c607877249e05a34c5293242f
b4041317 <b4041317@AC02-014D204612.hallam.shu.ac.uk>
2018-01-29 14:01:50 +0000

    SensorNetwork updated

--- src/cssd/subtask/pkg3/SensorNetwork.java
+++ src/cssd/subtask/pkg3/SensorNetwork.java
@@ -10,16 +10,80 @@ package cssd.subtask.pkg3;
  * @author b4041317
  */
 public class SensorNetwork {
-    private SensorStation[] Stations;
+    private SensorStation[] stations;
+    private int stationsCount;
+    private final int GROWBY;
     
+    SensorNetwork()
+    {
+        stationsCount =0;
+        GROWBY=10;
+        stations = new SensorStation[GROWBY];
+    }
     SensorStation[] getAllStations()
     {
-        return Stations;
+        return stations;
     }
-    
-    void addNewStation(SensorHandler sensor)    //
+    void addNewStation(SensorStation station)
     {
-        Stations[0].addNewSensor(sensor);
+        if (stationsCount > 0.9*stations.length)
+        {
+            resize();
+        }
+        stations[stationsCount] = station;
+        stationsCount++;
+    }
+    void addNewSensor(SensorHandler sensor) 
+    {
+        stations[0].addNewSensor(sensor);
     }
     
+    void addNewActuator(ActuatorHandler actuator)
+    {
+        stations[0].addNewActuator(actuator);
+    }
+    
+    void deleteStation (SensorStation station)
+    {
+        for(int i=0; i<stationsCount; i++)
+        {
+            if (stations[i].getId() == station.getId())
+            {
+                for (int j = i; j<stationsCount-1; j++)
+                {
+                    stations[j]=stations[j+1];
+                }
+                stationsCount--;
+                i=stationsCount;
+            }
+        }
+    }
+    void deleteSensor(SensorStation station, SensorHandler sensor)
+    {
+        for(int i=0; i<stationsCount; i++)
+        {
+            if (stations[i].getId() == station.getId())
+            {
+                stations[i].deleteSensor(sensor);
+                i=stationsCount;
+            }
+        }
+    }
+    void deleteActuator(SensorStation station, ActuatorHandler handles)
+    {
+        for(int i=0; i<stationsCount; i++)
+        {
+            if (stations[i].getId() == station.getId())
+            {
+                stations[i].deleteActuator(handles);
+                i=stationsCount;
+            }
+        }
+    }
+    private void resize()
+    {
+        SensorStation[] temp = new SensorStation[stations.length+GROWBY];
+        System.arraycopy(temp, 0, stations, 0, stationsCount);
+        stations = temp;
+    }    
 }
